1075ace4f98d08170aa30c72668d73553befb80d
Claire Namusoke <cnamusoke@stud.hs-bremen.de>
2025-02-24 00:20:28 +0100

    Test

---
+++ test.sql
@@ -0,0 +1,10 @@
+--Creating Database Shipping Emissions Domestically.
+create database ShippingEmissions
+
+---First Table Domestic Shipping By Country Emissions.
+CREATE TABLE DomesticShippingByCountry 
+( Id int primary key, iso3_Country varchar(10),
+Sector varchar (50),Subsector varchar (50),
+[Start-Time] datetime,[End-Time] datetime,
+Gas decimal(10,2))
+
