f096797e90be135a9f0af6361f994852646792c1
Claire Namusoke <cnamusoke@stud.hs-bremen.de>
2025-02-24 01:09:33 +0100

    test

--- test.sql
+++ test.sql
@@ -6,7 +6,9 @@ CREATE TABLE DomesticShippingByCountry
 ( Id int primary key, iso3_Country varchar(10),
 Sector varchar (50),Subsector varchar (50),
 [Start-Time] datetime,[End-Time] datetime,
-Gas decimal(10,2))
+Gas decimal(10,2),Emissions_quantity_units decimal(30,20),
+Created_date datetime, Modified_Date nvarchar (50)
+)
 
 
 --Comment test
